9817a15b2c6bd5c0150b393e70a7a516a5bc34d2
Lucio9182 <luucio789@gmail.com>
2016-11-15 20:45:50 -0200

    Add project files.

---
+++ Crudtst/inicio.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="inicio.aspx.cs" Inherits="Crudtst.empform" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div ng-app ="">
+        <br />
+
+        <asp:Label ID="Label1" runat="server" Text="Nome"></asp:Label>
+        <br />
+        <asp:TextBox ID="txtNome" runat="server" ></asp:TextBox>
+        <br />
+         <asp:Label ID="Label2" runat="server" Text="Telefone"></asp:Label>
+        <br />
+                <asp:TextBox ID="txttel" runat="server" Button="" server=""></asp:TextBox>
+        <br/>
+        <br/>
+        
+        <asp:Button ID="btnAdicionar" runat="server" Text="Adicionar" OnClick="btnAdicionar_Click" />
+        <asp:Button ID="btnAtualizar" runat="server" Text="Atualizar" OnClick="btnAtualizar_Click" />
+    
+                <br />
+        
+        <br />      
+        
+        <br />
+                <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false">
+
+            <Columns>
+                           <asp:BoundField DataField="id" HeaderText="Id" />
+             
+                <asp:BoundField DataField="nome" HeaderText="Nome" />
+                <asp:BoundField DataField="tel" HeaderText="Telefone" />
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:LinkButton ID="LinkButton1" runat="server" CommandArgument='<%# Eval("Id") %>' OnClick="lnk_Selecionar">Selecionar</asp:LinkButton>
+        <asp:LinkButton ID="LinkButton2" runat="server" CommandArgument='<%# Eval("Id") %>' OnClick="lnk_Apagar">Apagar</asp:LinkButton>
+        
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+
+
+
+        </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Crudtst/inicio.aspx.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+namespace Crudtst
+{
+    public partial class empform : System.Web.UI.Page
+    {
+
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-0AAOA16\\MSSQLSERVER02;Initial Catalog=Lucio;Integrated Security=True");
+        public int selecionado;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Mostrar_Valores();
+            }
+        }
+
+        protected void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            String str = "Insert into Crud values ('"+ txtNome.Text +"','"+txttel.Text+"')";
+            SqlCommand cmd = new SqlCommand(str, con);
+            cmd.ExecuteNonQuery();
+
+      
+
+
+            con.Close();
+            Mostrar_Valores();
+            txtNome.Text = "";
+            txttel.Text = "";
+            
+
+        }
+        protected void lnk_Selecionar(object sender, EventArgs e)
+        {
+            LinkButton btn = (LinkButton)sender;
+            Session["id"] = btn.CommandArgument;
+            GridViewRow linhaSelec = (GridViewRow)btn.NamingContainer;
+            int linhaIndex = linhaSelec.RowIndex; 
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from Crud", con);
+            DataTable dt = new DataTable();
+            SqlDataAdapter adp = new SqlDataAdapter(cmd);
+            
+            adp.Fill(dt);
+
+            con.Close();
+            if (dt.Rows.Count >= 0)
+            {
+
+                selecionado = (int)dt.Rows[linhaIndex]["id"];
+
+                txtNome.Text = dt.Rows[linhaIndex]["nome"].ToString(); 
+                txttel.Text = dt.Rows[linhaIndex]["tel"].ToString();
+                
+
+            }
+
+           
+
+        }
+
+        protected void lnk_Apagar(object sender, EventArgs e)
+        {
+            LinkButton btn = (LinkButton)sender;
+            Session["id"] = btn.CommandArgument;
+
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Delete From Crud WHERE Id = " + Session["id"] + ";", con);
+            DataTable dt = new DataTable();
+            SqlDataAdapter adp = new SqlDataAdapter(cmd);
+            adp.Fill(dt);
+            con.Close();
+            Mostrar_Valores();
+
+
+
+        }
+
+
+
+
+        public void Mostrar_Valores()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from Crud", con);
+            DataTable dt = new DataTable();
+            SqlDataAdapter adp = new SqlDataAdapter(cmd);
+            adp.Fill(dt);
+
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+
+            con.Close();
+
+        }
+
+        protected void btnAtualizar_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("UPDATE Crud SET Nome = '"+txtNome.Text+"', Tel = '"+txttel.Text+ "' WHERE Id = "+Session["id"]+";" , con);
+            DataTable dt = new DataTable();
+            SqlDataAdapter adp = new SqlDataAdapter(cmd);
+            adp.Fill(dt);
+            con.Close();
+            Mostrar_Valores();
+            }
+
+        protected void btnApagar_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Delete From Crud WHERE Id = " + Session["id"] + ";", con);
+            DataTable dt = new DataTable();
+            SqlDataAdapter adp = new SqlDataAdapter(cmd);
+            adp.Fill(dt);
+            con.Close();
+            Mostrar_Valores();
+        }
+    }
+}
